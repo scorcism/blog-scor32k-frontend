f8289aa1144104878d516e29ce0844eef28077b2
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-10-01 22:33:08 +0530

    add graphql

--- src/Scenes/Visitors/VisitorHome.jsx
+++ src/Scenes/Visitors/VisitorHome.jsx
@@ -1,5 +1,31 @@
+import { gql, useQuery } from '@apollo/client'
+
+const query = gql`
+query scor32kBlogs {
+    getBlogs {
+      id
+      blog
+      desc
+      imgUrl
+      published
+      title
+      userId
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+
+  `
 
 const VisitorHome=  () =>{
+
+    const { data, loading } = useQuery(query);
+
+    console.log(data)
+
     return (
         <>  
             Visitor home
